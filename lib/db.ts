8e3721c720e61bc3dccc1a2fc09c6108fde97ab6
romankarbivskyi <romankarbivskyi1@gmail.com>
2026-02-21 16:42:41 +0200

    chore: setup prisma with postgresql db

---
+++ lib/db.ts
@@ -0,0 +1,16 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+import { Pool } from "pg";
+import { PrismaClient } from "./generated/prisma/client";
+
+const connectionString = process.env.DATABASE_URL;
+const pool = new Pool({ connectionString });
+
+const adapter = new PrismaPg(pool);
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
